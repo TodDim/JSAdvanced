011ebb7d74b90923a47699f7f4cc4065a7285096
Todor Dimitrov <toshodimitrov@abv.bg>
2018-03-09 06:53:06 +0200

    commit

---
+++ 12 Lab Classes and Members/01. Rectangle Class.js
@@ -0,0 +1,10 @@
+class Rectangle {
+    constructor(width, height, color) {
+        [this.width, this.height, this.color] =
+            [width, height, color];
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
